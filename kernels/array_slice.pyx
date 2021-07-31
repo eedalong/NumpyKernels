f06bde1c128133df21633d487295d6a89a90b7c0
eedalong <ee_dalong@163.com>
2021-07-31 18:15:12 +0800

    add kernel

--- kernels/array_slice.pyx
+++ kernels/array_slice.pyx
@@ -2,18 +2,13 @@
 import numpy as np
 cimport numpy as np
 cimport cython
-from libcpp.map cimport map
-from libcpp.set cimport set
 from libcpp.unordered_set cimport unordered_set
 from libcpp.unordered_map cimport unordered_map
 from libcpp.vector cimport vector
 from libc.stdlib cimport rand, RAND_MAX
-
 from cython.parallel cimport prange
-import time
 
-from cython.parallel cimport prange
-cimport openmp
+
 @cython.boundscheck(False)
 @cython.wraparound(False)
 def long_2d_array_slice(np.ndarray[np.int64_t, ndim=2] array, np.ndarray[np.int64_t, ndim=1] indices):
@@ -32,7 +27,6 @@ def long_2d_array_slice(np.ndarray[np.int64_t, ndim=2] array, np.ndarray[np.int6
         for index in prange(total_indices, schedule="static", chunksize=30):
             row = indices_view[index]
             res_view[index, :] = array_view[row, :]
-    print(openmp.omp_get_num_threads())
     return res
 
 
