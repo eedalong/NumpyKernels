e8922702b67947e67fae5388bfe34dc1a27b5500
eedalong <ee_dalong@163.com>
2021-08-01 10:48:02 +0800

    fix kernel

--- kernels/array_pad.pyx
+++ kernels/array_pad.pyx
@@ -2,13 +2,13 @@
 import numpy as np
 cimport numpy as np
 from cython.parallel cimport prange
-
+import cython
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
 def long_2d_array_row_copy(np.ndarray[np.int64_t, ndim=2] target_array, np.ndarray[np.int64_t, ndim=2] source_array):
     cdef long long [:, :] target_view = target_array
-    cdef long long [:] source_view = source_array
+    cdef long long [:, :] source_view = source_array
     cdef Py_ssize_t total_count = source_array.shape[0]
     cdef long long length = source_array.shape[1]
     cdef Py_ssize_t index
@@ -16,14 +16,14 @@ def long_2d_array_row_copy(np.ndarray[np.int64_t, ndim=2] target_array, np.ndarr
     cdef Py_ssize_t col
     with nogil:
         for row in prange(total_count, schedule="static", chunksize=30):
-            target_view[row, :length] = source_array[row, :]
+            target_view[row, :length] = source_view[row, :]
     return target_array
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
 def long_2d_array_col_copy(np.ndarray[np.int64_t, ndim=2] target_array, np.ndarray[np.int64_t, ndim=2] source_array):
     cdef long long [:, :] target_view = target_array
-    cdef long long [:] source_view = source_array
+    cdef long long [:, :] source_view = source_array
     cdef Py_ssize_t total_count = source_array.shape[0]
     cdef long long length = source_array.shape[1]
     cdef Py_ssize_t index
@@ -31,6 +31,6 @@ def long_2d_array_col_copy(np.ndarray[np.int64_t, ndim=2] target_array, np.ndarr
     cdef Py_ssize_t col
     with nogil:
         for row in prange(total_count, schedule="static", chunksize=30):
-            target_view[row, :] = source_array[row, :]
+            target_view[row, :] = source_view[row, :]
     return target_array
 
